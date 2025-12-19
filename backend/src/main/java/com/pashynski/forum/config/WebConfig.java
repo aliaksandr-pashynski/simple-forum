893f91e8cfa41b3dac0c4091d60d669f75d28d7f
Aliaksandr Pashynski <844177@gmail.com>
2025-12-19 18:49:34 +0300

    split project into backend and frontend

---
+++ backend/src/main/java/com/pashynski/forum/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pashynski.forum.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Use patterns to allow all origins
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
