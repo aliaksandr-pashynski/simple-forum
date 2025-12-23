48c57a9bff301b27c010d7f388fb98a5b2c032a2
Aliaksandr Pashynski <844177@gmail.com>
2025-12-23 16:17:54 +0300

    add topics pageable

--- backend/src/main/java/com/pashynski/forum/feature/topic/TopicRepository.java
+++ backend/src/main/java/com/pashynski/forum/feature/topic/TopicRepository.java
@@ -1,13 +1,14 @@
 package com.pashynski.forum.feature.topic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TopicRepository extends JpaRepository<TopicEntity, UUID> {
 
-    List<TopicEntity> findAllByCategoryId(UUID categoryId);
+    Page<TopicEntity> findAllByCategoryId(UUID categoryId, Pageable pageable);
 }
